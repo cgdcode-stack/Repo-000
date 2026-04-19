031b0260edf3732aaedbdfb1a79290f029231db1
cgdcode-stack <cgdcode-stack@gmail.com>
2026-04-18 20:14:02 -0400

    modified:   scraper1.py

--- scraper1.py
+++ scraper1.py
@@ -8,4 +8,3 @@ series_data = response.json()
 print(f"Series Title: {series_data['series']['title']}")
 print(f"Frequency: {series_data['series']['frequency']}")
 print(f"Category: {series_data['series']['category']}")
-#helloworld
